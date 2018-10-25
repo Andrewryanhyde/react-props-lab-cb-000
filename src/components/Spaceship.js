b4359a906f8c24547f6097593b66835f92636249
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-25 07:52:27 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -5,7 +5,11 @@ class Spaceship extends React.component{
   render() {
     return {
       <p>{this.props.name}</p>
+      <p>{this.props.speed}</p>
+      <p>{this.props.hasRockets}</p>
+      <p>{this.props.colors}</p>
       
+
     }
   }
 }
