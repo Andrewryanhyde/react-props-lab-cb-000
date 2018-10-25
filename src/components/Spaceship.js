0fe1eee66049465a38ce5fac477e1627eaa3f5be
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-25 07:54:32 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 
 export default class Spaceship extends React.component{
   render() {
-    return {
+    return (
       <p>{this.props.name}</p>
       <p>{this.props.speed}</p>
       <p>{this.props.hasRockets}</p>
       <p>{this.props.colors.join(', ')}</p>
-    }
+    )
   }
 }
