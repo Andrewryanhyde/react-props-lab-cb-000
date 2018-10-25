5af2019feaaa1dc4227a6db47a341fe57082a6ce
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-10-25 07:51:21 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,4 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom' 
+import ReactDOM from 'react-dom'
 
-
+class Spaceship extends React.component{
+  render() {
+    return {
+      <p>this.props.name</p>
+    }
+  }
+}
